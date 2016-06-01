0f6c0053162c5ce0389f07c2f1f8fd8f1d56c76e
jrbtaylor <jasonrbtaylor@gmail.com>
2016-06-01 14:45:40 -0400

    early stopping

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ Created on Tue May 31 22:20:56 2016
 
 import numpy as np
 np.random.seed(1)  # for reproducibility
+import keras
 
 # Options
 opt = {'rows':105, 'cols':145}
@@ -24,6 +25,9 @@ cnn.compile(loss='categorical_crossentropy',optimizer='adadelta',
 # Train
 batch_size = 64
 nb_epoch = 100
-history = cnn.fit(x,y,batch_size=batch_size,nb_epoch=nb_epoch,verbose=1,validation_split=0.04,shuffle=True)
+
+
+earlyStopping=keras.callbacks.EarlyStopping(monitor='val_loss', patience=20, verbose=0, mode='auto')
+history = cnn.fit(x,y,batch_size=batch_size,nb_epoch=nb_epoch,verbose=1,callbacks=[earlyStopping],validation_split=0.04,shuffle=True)
 
 
